a59fcf67a2f5c05aab6192475afb4b221b24a899
Steven <stevenhuynh17@gmail.com>
2016-12-11 03:29:31 -0800

    Set up react and creating search component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import SearchBar from './Components/SearchBar.jsx';
+
+const API_KEY = 'AIzaSyB6Lh31Y4j7JZU3pNexU3Nnzu7DvLc4a1Y';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
