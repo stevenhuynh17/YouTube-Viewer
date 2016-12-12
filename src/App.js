9b2de457d5404e1b2e8702a806ec8a383f1204d6
Steven <stevenhuynh17@gmail.com>
2016-12-12 00:55:12 -0800

    Rendered img and title of videos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 import SearchBar from './Components/SearchBar.jsx';
+import VideoList from './Components/videoList.jsx';
 import YTSearch from 'youtube-api-search';
 import API_KEY from './config.js';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    })
+  }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
